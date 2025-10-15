e469ba2d8fdcfcd2f7f5a29e5682463d7a5a8dd2
diegotyner <diego.tyner@gmail.com>
2025-10-15 00:41:07 -0700

    changing relative links so files work w vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
